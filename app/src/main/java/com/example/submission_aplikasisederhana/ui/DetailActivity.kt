30feff856a9266bb630d044d38784a80e51d759a
M Fatih Maulana <muhammadfatihmaulana434@gmail.com>
2022-01-01 16:54:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/submission_aplikasisederhana/ui/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.submission_aplikasisederhana.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.submission_aplikasisederhana.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
